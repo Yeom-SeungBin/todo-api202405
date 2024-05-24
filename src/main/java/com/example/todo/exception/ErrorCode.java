d78fbed8c768a6aef555fa894f2ac5fe1c7fc17f
sseung <duaqkr2@naver.com>
2024-05-24 16:46:47 +0900

    #17. Access Token 갱신 요청 처리

--- src/main/java/com/example/todo/exception/ErrorCode.java
+++ src/main/java/com/example/todo/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "완료된 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_AUTH(HttpStatus.UNAUTHORIZED, "검증되지 않은 사용자입니다."),
     FORBIDDEN_AUTH(HttpStatus.FORBIDDEN, "권한이 없는 사용자입니다.");
 
